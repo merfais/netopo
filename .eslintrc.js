83da860c00cbc8242713e5142245cd76465ed62d
wenqing <wenqing@yunshan.net.cn>
2018-02-13 19:26:21 +0800

    编写打包脚本

---
+++ .eslintrc.js
@@ -0,0 +1,121 @@
+module.exports = {
+  root: true,
+  // baseConfig: false,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 6
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  plugins: [
+  ],
+  env: {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'amd': true,
+  },
+  globals: {
+  },
+  settings: {
+  },
+  // color: true,
+  rules: {
+    'import/no-unresolved': 0,
+    'import/newline-after-import': 0,
+    'import/imports-first': 0,
+    'import/extensions': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'import/no-named-as-default': 0,
+    'import/no-webpack-loader-syntax': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
+
+    //usage
+    'arrow-parens': [0],
+    'arrow-spacing': [1],
+    'arrow-body-style': [0],
+    'camelcase': [0],
+    'comma-dangle': [1, 'only-multiline'],
+    'comma-spacing': [1],
+    'consistent-return': [0, {"treatUndefinedAsUnspecified": true}],
+    'class-methods-use-this': [0],
+    'eol-last': [0, 'unix'],
+    'eqeqeq': [2],
+    'func-names': [1, 'never'],
+    'global-require': [0],
+    'guard-for-in': [2],
+    'key-spacing': [1],
+    'keyword-spacing': [1],
+    // SwitchCase https://github.com/eslint/eslint/issues/1797
+    'indent': [2, 2, {'SwitchCase': 1}],
+    'max-len': [1],
+    'new-cap': [1],
+    'newline-per-chained-call': [0, {
+      'ignoreChainWithDepth': 3
+    }],
+    'no-bitwise': [0, {
+      'allow': ['|', '&']
+    }],
+    'no-lonely-if': [0],
+    'no-mixed-operators': [0, {
+      'groups': [
+        ['+', '-', '*', '/', '%', '**'],
+        ['&', '|', '^', '~', '<<', '>>', '>>>'],
+        ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+        ['&&', '||'],
+        ['in', 'instanceof']
+      ],
+      'allowSamePrecedence': true
+    }],
+    'no-empty': [1, {
+      'allowEmptyCatch': true
+    }],
+    'no-empty-function': [2],
+    'no-else-return': [0],
+    'no-trailing-spaces': [2, {
+      'skipBlankLines': true
+    }],
+    'no-new': [1],
+    'no-multiple-empty-lines': [1, { 'max': 2, 'maxEOF': 0, 'maxBOF': 0 }],
+    'no-multi-str': [0],
+    'no-multi-spaces': [1, { ignoreEOLComments: true }],
+    'no-unused-vars': [2, {'args': 'none'}],
+    'no-unused-expressions': [2, {'allowShortCircuit': true}],
+    'no-underscore-dangle': [1, {'allowAfterThis': true}],
+    'no-unneeded-ternary': [1],
+    'no-restricted-syntax': [1, 'DebuggerStatement'],
+    'no-plusplus': [1, { 'allowForLoopAfterthoughts': true }],
+    'no-param-reassign': [0, {
+      'props': true
+    }],
+    'no-shadow': [0],
+    'object-shorthand': [0],
+    'object-curly-spacing': [1],
+    'object-curly-newline': [1, { "consistent": true }],
+    'operator-linebreak': [0],
+    'one-var': [1],
+    'one-var-declaration-per-line': [1, 'initializations'],
+    'prefer-arrow-callback': [0],
+    'prefer-const': [0],
+    'prefer-template': [0],
+    'prefer-spread': [1],
+    'prefer-destructuring': [0],
+    'padded-blocks': [0, "never"],
+    'quotes': [1, 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+    'quote-props': [1],
+    'radix': [1, 'as-needed'],
+    'spaced-comment': [1],
+    'space-infix-ops': [1],
+    'semi': [0],
+    'space-before-function-paren': [1, 'never'],
+    'space-before-blocks': [1],
+  },
+}
---
+++ build/config.js
@@ -0,0 +1,119 @@
+const path = require('path')
+const babel = require('rollup-plugin-babel')
+const eslint = require('rollup-plugin-eslint')
+const uglify = require('rollup-plugin-uglify')
+// const alias = require('rollup-plugin-alias')
+const commonjs = require('rollup-plugin-commonjs')
+// const replace = require('rollup-plugin-replace')
+const nodeResolve = require('rollup-plugin-node-resolve')
+// const flow = require('rollup-plugin-flow-no-whitespace')
+const progress = require('rollup-plugin-progress')
+const version = process.env.VERSION || require('../package.json').version
+
+const banner =
+  '/*!\n' +
+  ' * netopo.js v' + version + '\n' +
+  ' * (c) 2017-' + new Date().getFullYear() + ' bi wenqing\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+const resolve = p => path.resolve(__dirname, '../', p)
+
+const builds = {
+  // (CommonJS). Used by bundlers e.g. Webpack & Browserify
+  'prod-cjs': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.common.min.js'),
+    format: 'cjs',
+    env: 'production',
+    plugin: [uglify()],
+    banner
+  },
+  // (ES Modules). Used by bundlers that support ES Modules,
+  // e.g. Rollup & Webpack 2
+  'prod-esm': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.esm.min.js'),
+    format: 'es',
+    env: 'production',
+    plugin: [uglify()],
+    banner
+  },
+  // production build
+  'prod-umd': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.min.js'),
+    format: 'umd',
+    env: 'production',
+    plugin: [uglify()],
+    banner
+  },
+  // development build (CommonJS)
+  'full-cjs': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.common.js'),
+    format: 'cjs',
+    env: 'development',
+    banner
+  },
+  // development build  (ES Modules)
+  'full-esm': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.esm.js'),
+    format: 'es',
+    env: 'development',
+    banner
+  },
+  // development build.
+  'full-umd': {
+    entry: resolve('src/index.js'),
+    dest: resolve('dist/netopo.js'),
+    format: 'umd',
+    env: 'development',
+    banner
+  },
+}
+
+function genConfig (name) {
+  const opts = builds[name]
+  const config = {
+    input: opts.entry,
+    plugins: [
+      progress({
+        clearLine: false
+      }),
+      eslint(),
+      nodeResolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      commonjs(),
+    ].concat(opts.plugins || []),
+    output: {
+      file: opts.dest,
+      format: opts.format,
+      banner: opts.banner,
+      name: opts.moduleName || 'Netopo'
+    }
+  }
+
+  // if (opts.env) {
+  //   config.plugins.push(replace({
+  //     'process.env.NODE_ENV': JSON.stringify(opts.env)
+  //   }))
+  // }
+
+  Object.defineProperty(config, '_name', {
+    enumerable: false,
+    value: name
+  })
+
+  return config
+}
+
+if (process.env.TARGET) {
+  module.exports = genConfig(process.env.TARGET)
+} else {
+  exports.getBuild = genConfig
+  exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+}
